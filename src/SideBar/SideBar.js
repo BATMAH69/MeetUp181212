1cef571db23ae7c56c6df256de92366eaa842393
batmah <dev-www@ya.ru>
2018-12-12 02:54:28 +0300

    now it works more or less =))

--- src/SideBar/SideBar.js
+++ src/SideBar/SideBar.js
@@ -16,34 +16,17 @@ export default class SideBar extends React.Component {
       <Container>
         <Content>
           <Image
-            source={{
-              uri:
-                "https://raw.githubusercontent.com/GeekyAnts/NativeBase-KitchenSink/master/assets/drawer-cover.png"
-            }}
+            source={require('../../assets/logo.jpeg')}
             style={{
-              height: 120,
+              height: 180,
               width: "100%",
               alignSelf: "stretch",
               position: "absolute"
             }}
           />
-          <Image
-            square
-            style={{
-              height: 80,
-              width: 70,
-              position: "absolute",
-              alignSelf: "center",
-              top: 20
-            }}
-            source={{
-              uri:
-                "https://raw.githubusercontent.com/GeekyAnts/NativeBase-KitchenSink/master/assets/logo.png"
-            }}
-          />
           <List
             dataArray={routes}
-            contentContainerStyle={{ marginTop: 120 }}
+            contentContainerStyle={{ marginTop: 180 }}
             renderRow={data => {
               return (
                 <ListItem
